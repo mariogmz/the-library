89624802d61accd289ec3a34040f4c4c1f02673b
Mario Gómez <mariogomezmtz@gmail.com>
2017-10-23 00:11:06 -0500

    Add specs for only letters in name and author

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-  subject { build(:book) }
+  subject { build(:unavailable_book) }
 
   describe 'validations' do
     it { should validate_presence_of(:name) }
@@ -10,5 +10,19 @@ RSpec.describe Book, type: :model do
     it { should validate_presence_of(:category) }
     it { should belong_to(:category) }
     it { should belong_to(:user) }
+  end
+
+  describe '#name' do
+    it 'should only accept letters' do
+      subject.name = 'name123'
+      expect(subject).to be_invalid
+    end
+  end
+
+  describe '#author' do
+    it 'should only accept letters' do
+      subject.author = 'Author123'
+      expect(subject).to be_invalid
+    end
   end
 end
