ddcbd76cbbc6ded2daa5cb6986687329fce8228d
Mario Gómez <mariogomezmtz@gmail.com>
2017-10-23 00:59:25 -0500

    Revert "Add user specs"
    This reverts commit 175a15cc739bb185502b04832cc978aec7c3bad0.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,16 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-
-  describe 'validations' do
-    it { should validate_presence_of(:name) }
-    it { should validate_presence_of(:email) }
-
-    it 'should have a valid email' do
-      subject.email = 'hello.world.com'
-      expect(subject).to be_invalid
-      subject.email = 'test@mail.com'
-      expect(subject).to be_valid
-    end
-  end
+  pending "add some examples to (or delete) #{__FILE__}"
 end
