2213674c3db4d5048237a5827867e041ac1a88bb
Mario Gómez <mariogomezmtz@gmail.com>
2017-10-23 00:59:25 -0500

    Revert "Add user specs"
    This reverts commit 175a15cc739bb185502b04832cc978aec7c3bad0.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,2 @@
 class User < ApplicationRecord
-
-  validates_presence_of :name, :email
-  validates :name, format: { without: /[0-9]+/, message: "No numbers allowed" }
-  validates_format_of :email, with: /@/
 end
