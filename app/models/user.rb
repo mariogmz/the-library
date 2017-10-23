726c114eff62ab1789a24df9e5bb320f6a796595
Mario Gómez <mariogomezmtz@gmail.com>
2017-10-23 00:57:48 -0500

    Add user specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+
+  validates_presence_of :name, :email
+  validates :name, format: { without: /[0-9]+/, message: "No numbers allowed" }
+  validates_format_of :email, with: /@/
 end
