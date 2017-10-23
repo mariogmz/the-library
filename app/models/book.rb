6edbd43672657cc86beb70afa6e1bb86d70e56e4
Mario Gómez <mariogomezmtz@gmail.com>
2017-10-23 00:11:23 -0500

    Add specs and validations for Book

---
+++ app/models/book.rb
@@ -0,0 +1,7 @@
+class Book < ApplicationRecord
+
+  validates_presence_of :name, :author, :published, :category
+  validates :name, :author, format: { with: /\A[a-zA-Z]+\z/, message: "Only letters allowed" }
+  belongs_to :category
+  belongs_to :user
+end
