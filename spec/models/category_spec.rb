08b152047b89d92d8bf79a54674d7a8b9d6fc5a9
Mario Gómez <mariogomezmtz@gmail.com>
2017-10-23 00:59:25 -0500

    Revert "Add Category model and tests"
    This reverts commit c7a9f4ed0d04e17ba234c20a9fdcc25b41c32aee.

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,20 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  subject { build(:category) }
-
-  describe 'validations' do
-    it { should validate_presence_of(:name) }
-    it { should validate_presence_of(:description) }
-    it { should have_many(:books) }
-  end
-
-  describe '#name' do
-    it 'should only accept letters' do
-      subject.name = 'name 123'
-      expect(subject).to be_invalid
-      subject.name = 'Foo Bar'
-      expect(subject).to be_valid
-    end
-  end
+  pending "add some examples to (or delete) #{__FILE__}"
 end
