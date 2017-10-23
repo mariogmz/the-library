12d62e3ae60700950c62f28d1970817172b5462e
Mario Gómez <mariogomezmtz@gmail.com>
2017-10-23 00:39:06 -0500

    Add Category model and tests

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,6 @@
 class Category < ApplicationRecord
+
+  validates_presence_of :name, :description
+  validates :name, format: { without: /[0-9]+/, message: "Only letters allowed" }
+  has_many :books
 end
