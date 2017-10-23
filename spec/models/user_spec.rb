175a15cc739bb185502b04832cc978aec7c3bad0
Mario Gómez <mariogomezmtz@gmail.com>
2017-10-23 00:57:48 -0500

    Add user specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:email) }
+
+    it 'should have a valid email' do
+      subject.email = 'hello.world.com'
+      expect(subject).to be_invalid
+      subject.email = 'test@mail.com'
+      expect(subject).to be_valid
+    end
+  end
 end
