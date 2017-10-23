0342b75fb1fc804b9064f49b3f53f598b31fda37
Mario Gómez <mariogomezmtz@gmail.com>
2017-10-23 00:59:25 -0500

    Revert "Add specs and validations for Book"
    This reverts commit 6edbd43672657cc86beb70afa6e1bb86d70e56e4.

--- app/models/book.rb
+++
@@ -1,7 +0,0 @@
-class Book < ApplicationRecord
-
-  validates_presence_of :name, :author, :published, :category
-  validates :name, :author, format: { with: /\A[a-zA-Z]+\z/, message: "Only letters allowed" }
-  belongs_to :category
-  belongs_to :user
-end
